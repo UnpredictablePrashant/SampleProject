a685d0860950a17470df48d53908a3b7a42ddfc7
Prashant Kumar Dey <prashantsavior@gmail.com>
2024-03-02 09:59:53 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, request, jsonify
-from flask_pymongo import PyMongo
+
 
 app = Flask(__name__)
 @app.route('/')
@@ -7,4 +7,4 @@ def hello_world():
     return 'This is my first API call!'
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=8080)
+    app.run(host='0.0.0.0', port=80)
